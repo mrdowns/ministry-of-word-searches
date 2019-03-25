682155576c8d0202f2c3218b9ab7e04cc31f564c
Matt Downs <matt.downs@gmail.com>
2019-03-24 20:51:47 -0400

    fixed the algo so it wasn't impossibly slow
    Was putting way too many options into the first array, making the
    intersection calc incredibly slow at the scale of a million

--- ministry.js
+++ ministry.js
@@ -1,22 +1,43 @@
-const intersection = require('array-intersection')
+const intersection = require('intersect')
 
 var mps = [
-	{ name: 'Trudeau', period: 23, lookingFor: 14, series: [] },
-	{ name: 'Campbell', period: 19, lookingFor: 16, series: [] },
-	{ name: 'Diefenbaker', period: 13, lookingFor: 7, series: [] }
+	{ name: 'Trudeau', period: 23, lookingFor: 17, series: [] },
+	{ name: 'Campbell', period: 19, lookingFor: 1, series: [] },
+	{ name: 'Turner', period: 17, lookingFor: 6, series: [] },
+	{ name: 'Diefenbaker', period: 13, lookingFor: 12, series: [] },
+	{ name: 'Bennett', period: 11, lookingFor: 2, series: [] },
+	{ name: 'Laurier', period: 7, lookingFor: 1, series: [] }
+	// { name: 'Bowell',  period: 5, lookingFor: 2, series: [] },
+	// { name: 'Abbott', period: 3, lookingFor: 3, series: [] }
 ];
 
-const iterations = 9999;
+const iterations = 9999999;
 
 mps.forEach(mp => {
-	console.log(mp.name, '...')
-	for(var i = 0; i < iterations; i++) {
-		mp.series.push((i * mp.period) + mp.lookingFor);
-		// console.log((i * mp.period) + mp.lookingFor);
+	console.log(mp.name, '...');
+	// for(var i = 0; i < iterations; i++) {
+	var i = 0;
+	var looking = 0;
+	while((looking = (i * mp.period) + mp.lookingFor) < iterations) {
+		mp.series.push(looking);
+		i++;
 	}
+	console.log('pushed', mp.series.length);
 });
 
-const poodles = intersection.apply(null, mps.map(mp => mp.series));
+var results = mps[0].series;
+
+for (var i = 1; i < mps.length; i++) {
+	console.log('intersecting', results.length, 'and', mps[i].series.length);
+	results = intersection.big(results, mps[i].series);
+}
+
+// const poodles = intersect([mps[0].series, mps[1].series, mps[2].series]);
+// const poodles = intersection.big(mps[0].series, mps[1].series);
+
+// const poodles = intersect(mps.map(mp => mp.series));
+
+// const poodles = intersection.apply(null, mps.map(mp => mp.series));
 
 console.log('ANSWER');
-console.log(poodles);
+console.log(results);
